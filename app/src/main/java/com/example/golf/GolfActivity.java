18e2bd175aa9ed2fc43b3b6d8882a2c970a7a94d
Fabian Fischer <fabfis@az.zli.ch>
2021-11-19 10:30:22 +0100

    project nearly finished, borders missing

--- app/src/main/java/com/example/golf/GolfActivity.java
+++ app/src/main/java/com/example/golf/GolfActivity.java
@@ -1,9 +1,14 @@
 package com.example.golf;
 
+import static java.util.Collections.max;
+import static java.util.Collections.min;
+
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.graphics.Canvas;
 import android.graphics.Paint;
 import android.hardware.Sensor;
@@ -16,6 +21,7 @@ import android.os.Bundle;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 
+import java.util.Random;
 import java.util.Timer;
 import java.util.TimerTask;
 
@@ -26,17 +32,31 @@ public class GolfActivity extends AppCompatActivity implements SurfaceHolder.Cal
     private Sensor magneticField;
     private Timer timer;
     private Paint player;
+    private Paint time;
     private Paint goal;
     Double hMovement = 0.0;
     Double vMovement = 0.0;
     float[] mGravity;
     float[] mGeomagnetic;
+    float testv = 0;
+    float testh = 0;
+    boolean noGoal = true;
+    float hGoal = 0;
+    float vGoal = 0;
+    int score = 0;
+    int frameCounter = 0;
+    int actualTime = 10;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_golf);
+
+        //Time
+        this.time = new Paint();
+        time.setTextSize(60f);
+        time.setColor(getColor(R.color.black));
 
         //Player
         this.player = new Paint();
@@ -63,9 +83,16 @@ public class GolfActivity extends AppCompatActivity implements SurfaceHolder.Cal
         this.timer = new Timer();
         this.timer.schedule(new TimerTask() {
             int size = 100;
+
             @Override
             public void run() {
-                tryDrawing(holder, size);
+                frameCounter++;
+                if(frameCounter % 62.5 < 1) {
+                    actualTime--;
+                }
+                float h = Float.parseFloat(String.valueOf(hMovement));
+                float v = Float.parseFloat(String.valueOf(vMovement));
+                tryDrawing(holder, size, h, v);
             }
         }, 0, 16); // 1000ms / 16ms = 62.5 frames per second
     }
@@ -79,20 +106,58 @@ public class GolfActivity extends AppCompatActivity implements SurfaceHolder.Cal
         this.timer.cancel();
     }
 
-    private void tryDrawing(SurfaceHolder holder, int ticks) {
+    private void tryDrawing(SurfaceHolder holder, int ticks, float h, float v) {
         Canvas canvas = holder.lockCanvas();
-        if(canvas != null) {
-            draw(canvas, ticks);
+        if (canvas != null) {
+            draw(canvas, ticks, h, v);
             holder.unlockCanvasAndPost(canvas);
         }
     }
 
-    private void draw(final Canvas canvas, int ticks) {
-        float h = Float.parseFloat(String.valueOf(hMovement));
-        float v = Float.parseFloat(String.valueOf(vMovement));
+    private void draw(final Canvas canvas, int ticks, float h, float v) {
+        testv = testv + v;
+        testh = testh + h;
+
         canvas.drawColor(getColor(android.R.color.white));
-        canvas.drawCircle(canvas.getWidth()/2 + h, canvas.getHeight()/2 + v, ticks % 200, this.player);
-        canvas.drawCircle(canvas.getWidth()/2 + 100, canvas.getHeight()/2 + 100, ticks % 200, this.goal);
+        canvas.drawCircle(canvas.getWidth() / 2 + testh, canvas.getHeight() / 2 + testv, ticks % 200, this.player);
+        canvas.drawText(String.valueOf(actualTime), canvas.getWidth() - 100, canvas.getHeight() - 100, this.time);
+        canvas.drawText(String.valueOf(score), canvas.getWidth() + 100, canvas.getHeight() - 100, this.time);
+
+        //check for collision
+        double dx = (Double.parseDouble(String.valueOf(canvas.getWidth() / 2 + vGoal)) + (ticks % 200)) - (Double.parseDouble(String.valueOf(canvas.getWidth() / 2 + testh)) + (ticks % 200));
+        double dy = (Double.parseDouble(String.valueOf(canvas.getHeight() / 2 + hGoal)) + (ticks % 200)) - (Double.parseDouble(String.valueOf(canvas.getHeight() / 2 + testv)) + (ticks % 200));
+        double distance = Math.sqrt(dx * dx + dy * dy);
+
+        if (distance < (ticks % 200) + (ticks % 200)) {
+            noGoal = true;
+            actualTime = 10;
+            score++;
+            System.out.println(score);
+        }
+
+
+        if (isGameOver()){
+            SharedPreferences mPrefs = getSharedPreferences("highscore", 0);
+            String highscoreString = mPrefs.getString("highscore", "0");
+            if (Integer.parseInt(highscoreString) < score){
+                SharedPreferences.Editor mEditor = mPrefs.edit();
+                mEditor.putString("highscore", Integer.toString(score)).apply();
+            }
+            changeView(Integer.toString(score));
+        }
+
+        if (noGoal){
+            hGoal = Float.parseFloat(Integer.toString(hOfGoal()));
+            vGoal = Float.parseFloat(Integer.toString(vOfGoal()));
+            noGoal = false;
+        }
+        if (!noGoal){
+            canvas.drawCircle(canvas.getWidth() / 2 + vGoal, canvas.getHeight() / 2 + hGoal, ticks % 200, this.goal);
+        }
+
+    }
+    public boolean isGameOver(){
+        return actualTime == 0;
     }
 
     @Override
@@ -130,54 +195,79 @@ public class GolfActivity extends AppCompatActivity implements SurfaceHolder.Cal
 
     }
 
-    public void calculateMovement(double pitch, double roll){
-        if (pitch >= -20 && pitch < 0){
+    public void calculateMovement(double pitch, double roll) {
+        if (pitch == 0 && roll == 0) {
+            hMovement = 0.0;
+            vMovement = 0.0;
+        }
+        if (pitch >= -20 && pitch < 0) {
             hMovement = 0.325;
         }
-        if (pitch >= -40 && pitch < -20){
+        if (pitch >= -40 && pitch < -20) {
             hMovement = 0.65;
         }
-        if (pitch >= -60 && pitch < -40){
+        if (pitch >= -60 && pitch < -40) {
             hMovement = 0.976;
         }
-        if (pitch >= -80 && pitch < -60){
+        if (pitch >= -80 && pitch < -60) {
             hMovement = 1.3;
         }
-        if (pitch >= 20 && pitch > 0){
+        if (pitch >= 20 && pitch > 0) {
             hMovement = -0.325;
         }
-        if (pitch >= 40 && pitch > 20){
+        if (pitch >= 40 && pitch > 20) {
             hMovement = -0.65;
         }
-        if (pitch >= 60 && pitch > 40){
+        if (pitch >= 60 && pitch > 40) {
             hMovement = -0.976;
         }
-        if (pitch >= 80 && pitch > 60){
+        if (pitch >= 80 && pitch > 60) {
             hMovement = -1.3;
         }
-        if (roll >= -20 && roll < 0){
+        if (roll >= -20 && roll < 0) {
             vMovement = 0.325;
         }
-        if (roll >= -40 && roll < -20){
+        if (roll >= -40 && roll < -20) {
             vMovement = 0.65;
         }
-        if (roll >= -60 && roll < -40){
+        if (roll >= -60 && roll < -40) {
             vMovement = 0.976;
         }
-        if (roll >= -80 && roll < -60){
+        if (roll >= -80 && roll < -60) {
             vMovement = 1.3;
         }
-        if (roll >= 20 && roll > 0){
+        if (roll >= 20 && roll > 0) {
             vMovement = -0.325;
         }
-        if (roll >= 40 && roll > 20){
+        if (roll >= 40 && roll > 20) {
             vMovement = -0.65;
         }
-        if (roll >= 60 && roll > 40){
+        if (roll >= 60 && roll > 40) {
             vMovement = -0.976;
         }
-        if (roll >= 80 && roll > 60){
+        if (roll >= 80 && roll > 60) {
             vMovement = -1.3;
         }
+    }
+
+    public int vOfGoal() {
+        Integer[] integers = {100, 150, 200, 250, 300, 350, 400, 450,  500, 550,  600, 650, 700, 750, -100, -150, -200, -250, -300, -350, -400, -450,  -500, -550,  -600, -650, -700, -750};
+        Random rand = new Random();
+        int upperbound = integers.length;
+        int v = rand.nextInt(upperbound);
+        return integers[v];
+
+    }
+    public int hOfGoal() {
+        Integer[] integers = {25, 50, 75, 100, 125, 150, 175, 200, 225, 250, 275, 300, 325, -25, -50, -75, -100, -125, -150, -175, -200, -225, -250, -275, -300, -325};
+        Random rand = new Random();
+        int upperbound = integers.length;
+        int h = rand.nextInt(upperbound);
+        return integers[h];
+    }
+    public void changeView(String score) {
+        Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+        intent.putExtra("score",score);
+        startActivity(intent);
     }
 }
--- app/src/main/java/com/example/golf/MainActivity.java
+++ app/src/main/java/com/example/golf/MainActivity.java
@@ -3,12 +3,15 @@ package com.example.golf;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    int score;
+    String highscoreS;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,6 +19,20 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Button play = (Button) findViewById(R.id.play);
         TextView highscore = (TextView) findViewById(R.id.highscore);
+        Bundle extras = getIntent().getExtras();
+
+        SharedPreferences mPrefs = getSharedPreferences("highscore", 0);
+        String highscoreString = mPrefs.getString("highscore", "0");
+
+        highscore.setText(highscoreString);
+
+        highscore.setText(highscoreString);
+        if (extras != null) {
+            String scoreS = extras.getString("score");
+            score = Integer.parseInt(scoreS);
+        }
+
+
         play.setOnClickListener(v -> {
             changeView();
         });
